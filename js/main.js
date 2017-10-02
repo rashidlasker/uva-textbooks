a62f478b33fd02f483d68e364093cc05a64ff536
datacub <yt2c@virginia.edu>
2017-10-01 21:10:49 -0400

    Added About and Login Page and changed some formatting poorly. Plz teach me how to be better.

--- js/main.js
+++ js/main.js
@@ -4,9 +4,11 @@ if(loggedin){
     document.getElementById("navbuttons").innerHTML = 
         '<button class="navbutton" href="#">Sell</button>\n' +
         '<button class="navbutton" href="#">Account</button>\n' +
+        '<button class="navbutton"><a href="about.html" style="text-decoration: none">About Us</a></button>\n'+
         '<button class="navbutton" href="#">Logout</button>\n';
 }else{
     document.getElementById("navbuttons").innerHTML = 
-        '<button class="navbutton" href="#">Sell</button>\n' +
-        '<button class="navbutton" href="#">Login</button>\n';
+        '<button class="navbutton">Sell</button>\n' +
+        '<button class="navbutton"><a href="about.html" style="text-decoration: none">About Us</a></button>\n'+
+        '<button class="navbutton"><a href="login.html" style="text-decoration: none">Login</a></button>\n';
 }
